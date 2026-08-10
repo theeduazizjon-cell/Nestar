00be6f2fb0923311c8377a1485d8a575fdfef79c
Alex <theeduazizjon@gmail.com>
2026-08-10 23:18:50 +0900

    feat: Integration of GraphQlModule and developed first GraphQl API

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT_API ?? 3007); // on the video it was still 3000 but my server didn't work 
+  const app = await NestFactory.create(AppModule); // AppModule is an argument here
+  await app.listen(process.env.PORT_API ?? 3007); // on the video it was still 3000 but my server didn't work  http://localhost:3007/
 }
 bootstrap();
--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -3,6 +3,6 @@ import { NestarBatchModule } from './nestar-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(NestarBatchModule);
-  await app.listen(process.env.port ?? 3008); // not matching with video server localhost , still 3000
+  await app.listen(process.env.port ?? 3008); // not matching with video server localhost , still 3000. http://localhost:3008/
 }
 bootstrap();
