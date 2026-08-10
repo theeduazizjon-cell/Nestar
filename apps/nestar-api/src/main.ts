70f389567737f72998e921e43ba079ccf0def714
Alex <theeduazizjon@gmail.com>
2026-08-10 22:47:05 +0900

    fix: Installation of packages & Modification of format standards

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT_API ?? 3007);
+  await app.listen(process.env.PORT_API ?? 3007); // on the video it was still 3000 but my server didn't work 
 }
 bootstrap();
