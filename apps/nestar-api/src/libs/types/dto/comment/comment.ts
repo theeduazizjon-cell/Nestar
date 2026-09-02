5bc147cf750a801182d9d0da93e780a9fca96841
Alex <theeduazizjon@gmail.com>
2026-09-02 10:23:25 +0900

    feat: Configure comment module & create comment related resolver and service model

--- apps/nestar-api/src/libs/types/dto/comment/comment.ts
+++ apps/nestar-api/src/libs/types/dto/comment/comment.ts
@@ -1,5 +1,5 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { ObjectId } from 'mongoose';
+import type { ObjectId } from 'mongoose';
 import { CommentGroup, CommentStatus } from '../../enums/comment.enum';
 import { Member, TotalCounter } from '../member/member';
 
